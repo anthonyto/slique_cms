9f6a79f99c6f7400b8eef7c32708601d71ce58d4
Anthony To <to.anthony93@gmail.com>
2017-02-11 17:50:21 -0800

    Set up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+account = Account.create(
+  name: 'L&L Hawaiian BBQ - La Jolla',
+  client: 'Anthony To, anthony123@example.com',
+  services: 'Account management, creative development, Yelp',
+  instagram: 'Username: @llbbqlajolla Password: password123',
+  facebook: 'https://www.facebook.com/hawaiianbarbecue/',
+  yelp: 'Username: llhb@example.com Password: password123',
+  slique_email: 'llbbq@slique.com',
+  pr_partner: 'Some partner',
+  ubest: false,
+  fivestarboss: true,
+  facebook_ad: true,
+  facebook_ad_notes: 'Budget is $300/month',
+  correspondence: '1/23 Jeanie called client about catering',
+  messaging_timeline_q1: 'Catering',
+  messaging_timeline_q2: 'Delivery Services',
+  messaging_timeline_q3: 'Back to school',
+  messaging_timeline_q4: 'Catering/gift card for holidays',
+  priorities: 'Hiring more people',
+  promotions: "Valentine's day",
+  notes: 'This is one of our biggest clients'
+)
+
+account.tasks.create(name: 'Research PR firm', requested: Date.today, completed: false)
+account.tasks.create(name: 'New promo campaign', requested: Date.yesterday, completed: false)
+
+account.creative_developments.create(design_name: 'Hiring design', requested: Date.yesterday, completed: false)
+account.creative_developments.create(design_name: 'Poster designs', requested: Date.today, completed: false)
